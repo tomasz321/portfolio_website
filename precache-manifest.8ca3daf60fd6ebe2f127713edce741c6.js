0bfd49f62edb6bdf0ffe3d3eeea3847c5184d5e1
tomasz.dentko <tomasz.dentko@jeppesen.com>
2020-10-15 11:56:37 +0200

    Updates

---
+++ precache-manifest.8ca3daf60fd6ebe2f127713edce741c6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "14b76bdadc88fb648639",
+    "url": "/portfolio_website/static/css/main.d317dcae.chunk.css"
+  },
+  {
+    "revision": "14b76bdadc88fb648639",
+    "url": "/portfolio_website/static/js/main.14b76bda.chunk.js"
+  },
+  {
+    "revision": "98327c965e1d89457cdd",
+    "url": "/portfolio_website/static/js/1.98327c96.chunk.js"
+  },
+  {
+    "revision": "a11c90defcf311fda18b",
+    "url": "/portfolio_website/static/js/runtime~main.a11c90de.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio_website/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "37d029f22f846a612bb313f784ca74da",
+    "url": "/portfolio_website/static/media/TD_cv.37d029f2.pdf"
+  },
+  {
+    "revision": "1e83f7103d15e32357b2dc768106fac0",
+    "url": "/portfolio_website/static/media/postgresql-original.1e83f710.svg"
+  },
+  {
+    "revision": "edcc6a50f345fb3fbc4ebea643c04db6",
+    "url": "/portfolio_website/static/media/bitbucket-original.edcc6a50.svg"
+  },
+  {
+    "revision": "c4ce29f3d8d818e21c893bde4d815089",
+    "url": "/portfolio_website/static/media/bootstrap-plain.c4ce29f3.svg"
+  },
+  {
+    "revision": "792f835aac1501f579814406af2e0eae",
+    "url": "/portfolio_website/static/media/django-original.792f835a.svg"
+  },
+  {
+    "revision": "e618708b41bf4b0b9ee2720423e7a242",
+    "url": "/portfolio_website/static/media/docker-original.e618708b.svg"
+  },
+  {
+    "revision": "c02b7da93fea7f472b89583e23f12759",
+    "url": "/portfolio_website/static/media/git-original.c02b7da9.svg"
+  },
+  {
+    "revision": "4097a5be85bf4c4e64ec52f389ea039d",
+    "url": "/portfolio_website/static/media/github-original.4097a5be.svg"
+  },
+  {
+    "revision": "80922f3d5f71dc3e4723b37ffa538afa",
+    "url": "/portfolio_website/static/media/gitlab-original.80922f3d.svg"
+  },
+  {
+    "revision": "7997c4244e7a8cf2b160803e1c741502",
+    "url": "/portfolio_website/static/media/html5-original.7997c424.svg"
+  },
+  {
+    "revision": "66517ded2b3a6bef72a9c7fc9a1fc931",
+    "url": "/portfolio_website/static/media/java-original.66517ded.svg"
+  },
+  {
+    "revision": "0154328fc3e7bfcdb3d9ca42a6f848b1",
+    "url": "/portfolio_website/static/media/javascript-plain.0154328f.svg"
+  },
+  {
+    "revision": "2bf747f09a4f9135fbd3032432f4158b",
+    "url": "/portfolio_website/static/media/linux-original.2bf747f0.svg"
+  },
+  {
+    "revision": "c65b5a946a3b31ee10d467fd78f56c45",
+    "url": "/portfolio_website/static/media/pycharm-original.c65b5a94.svg"
+  },
+  {
+    "revision": "3b84eb4026f0c704e7e3d16972bb0e3d",
+    "url": "/portfolio_website/static/media/python-original.3b84eb40.svg"
+  },
+  {
+    "revision": "bd4a3d9256994fb686702e51ebec4816",
+    "url": "/portfolio_website/static/media/slack-original.bd4a3d92.svg"
+  },
+  {
+    "revision": "734e3d7764fe5f770d6615e505068437",
+    "url": "/portfolio_website/index.html"
+  }
+];
